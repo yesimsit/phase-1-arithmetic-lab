b92180e72ab6727fc04259ea99e01d1da513316b
yesim <sityesim@gmail.com>
2022-04-20 00:31:47 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1 = 60;
+let num2 =2;
+let multiply = num1 +num2;
+console.log(multiply);
+
+
+
+const random = Math.floor(Math.random() * (10 - 1)) + 1;
+console.log(random);
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+
+const max=Math.max(1, 2, 3, 14, 5,20);
+console.log(max);
